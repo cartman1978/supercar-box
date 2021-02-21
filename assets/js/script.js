36b86abaac7050045cc113d18958b9c6ef2fc17d
Fabio Torti <email>
2021-02-21 10:51:15 +0000

    added javascript mouseenter to cahnge table color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
-
+// Shoew/Hide table on mobile only
 function tableMobile(id) {
     let e = document.getElementById(id);
     e.style.display =
         ((e.style.display != 'none') ? 'none' : 'block');
+}
+
+
+// Onmouseenter change table size and color
+function bigTable(size) {
+    size.style.color = '#000000';
+    size.style.fontWeight = 'bold';
+    size.style.backgroundColor = '#F4CA04';
+    document.getElementById('displaytable').style.borderColor = 'yellow';
 }
